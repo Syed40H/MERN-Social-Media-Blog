381ef02d9248acc338275b5fe1ac1b1b0d4c1f87
Syed40A <72885427+Syed40H@users.noreply.github.com>
2024-08-19 18:22:36 -0400

    mogodb

--- api/index.js
+++ api/index.js
@@ -1,5 +1,7 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
+mongoose.connect(`mongodb+srv://abdul40hadi:<${process.env.PASSWORD}>@mern-blog.m00wv.mongodb.net/?retryWrites=true&w=majority&appName=mern-blog`)
 const app = express();
 
 app.listen(3000, () => {
