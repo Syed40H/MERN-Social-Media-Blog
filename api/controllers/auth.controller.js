5f6b33a971a56b333296c9d186951cefd426d725
Syed40A <72885427+Syed40H@users.noreply.github.com>
2024-09-12 10:08:35 -0400

    fix auth control

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,1 +1,27 @@
-n  
+import User from '../models/user.model.js';
+import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
+
+export const signup = async (req, res, next) => {
+  const { username, email, password } = req.body;
+
+  if (!email || !password || email === '' || password === '') {
+    return res.status(400).json({ message: 'All fields are required' });
+  }
+
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+
+
+  const newUser = new User({
+    username,
+    email,
+    password: hashedPassword,
+  });
+
+  try {
+    await newUser.save();
+    res.json('Signup successful');
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
